063df7bc2c1deeb0601f2bd586872823bfce72a3
MohitRane8 <mohit.rane@colorado.edu>
2019-09-04 02:39:24 -0600

    removed redundant code in finder.sh

--- finder.sh
+++ finder.sh
@@ -22,25 +22,6 @@ fi
 
 # find number of files in given directory
 NUM_OF_FILES=$( find $filesdir -type f | wc -l )
-echo number of files is $NUM_OF_FILES
-echo
-
-: '
-MATCHING_TOTAL_LINES=0
-find $filesdir -type f > files_path.txt
-
-while read -r line;
-do
-    MATCHING_LINES=$(cat $line | grep $searchstr | wc -l)
-    echo $MATCHING_LINES :: $line
-    ((MATCHING_TOTAL_LINES+=MATCHING_LINES))
-done < "files_path.txt"
-echo 
-echo $MATCHING_TOTAL_LINES lines found
-echo
-
-rm files_path.txt
-'
 
 # find total matching lines
 MATCHING_TOTAL_LINES=0
@@ -48,10 +29,8 @@ MATCHING_TOTAL_LINES=0
 for line in $(find $filesdir -type f)
 do
     MATCHING_LINES=$(cat $line | grep $searchstr | wc -l)
-    echo $MATCHING_LINES :: $line
+    #echo $MATCHING_LINES :: $line
     ((MATCHING_TOTAL_LINES+=MATCHING_LINES))
 done
-echo
-echo $MATCHING_TOTAL_LINES
 
 echo The number of files are $NUM_OF_FILES and the number of matching lines are $MATCHING_TOTAL_LINES
